56f40113cb56d64662db048fe7eaa995c41e701e
JeniJ23 <145951180+JeniJ23@users.noreply.github.com>
2023-10-23 18:47:52 -0700

    Update parkingGarage.py
    Updated changes on my end

--- parkingGarage.py
+++ parkingGarage.py
@@ -1,16 +1,19 @@
-# Start Your Code here
 class ParkingGarage:
-    def __init__(self, total_spaces = 50):
-        self.tickets = list(range(1, total_spaces + 1))
-        self.parkingSpaces = list(range(1, total_spaces + 1))
-        self.currentTicket = {}
+    def __init__(self, total_tickets, total_parking_spaces):
+        self.total_tickets = total_tickets
+        self.total_parking_spaces = total_parking_spaces
+        self.current_ticket = {}
+        self.tickets = [i for i in range(1, total_tickets + 1)]
+        self.parking_spaces = [i for i in range(1, total_parking_spaces + 1)]
 
-    def takeTicket(self):
-        
-        ticket = self.tickets.pop(0)
-        parking_space = self.parkingSpaces.pop(0)
-        self.currentTicket = {'ticket': ticket, 'parking_space': parking_space, 'paid': False}
-        print(f"Ticket {ticket} issued. Parking space {parking_space} is available.")
+def take_ticket(self):
+        if self.tickets:
+            ticket_number = self.tickets.pop(0)
+            parking_space = self.parking_spaces.pop(0)
+            self.current_ticket = {"ticket_number": ticket_number, "paid": False, "parking_space": parking_space}
+            print(f"Ticket {ticket_number} issued. Parking space {parking_space} is available.")
+        else:
+            print("Sorry,Garage is Full.")
 
     def payParking(self):
 
